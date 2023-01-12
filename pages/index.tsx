886a414abb604c1275a20c6ad16f1ad60010b16b
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-01-12 15:03:51 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,7 +101,7 @@ const Pokedex: React.FC = ()=>{
     <>
       <Head>
         <title>Pokedex</title>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
       </Head>
       <GlobalStyle></GlobalStyle>
       <Wallpaper></Wallpaper>
